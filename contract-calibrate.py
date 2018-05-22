efc06a52d44bb163da0c740eb887e4954bf1994f
James Steven <Jamessteven301@gmail.com>
2018-05-22 14:36:19 -0400

    Update contract-calibrate.py

--- contract-calibrate.py
+++ contract-calibrate.py
@@ -1,1 +1,23 @@
+##########################################
+#--------------calibrate.py--------------#
+##########################################
 
+#-------------Description----------------#
+#Poles imaging hardware for 300 ms and detects chess 
+
+#-----------visual elements--------------#
+    #@button - lable: "Calibrate"
+        #Calls method: calibrate()
+    #@button - lable: "Execute"
+        #Calls method: execute()
+    #@button - lable: "Reset"
+        #Calls method: reset()
+    #@button - lable: "Interface"
+        #Calls method: interface()
+
+#-------------Description----------------#
+# Just make these four buttons in a basic gui window.
+# All they have to do is call ghost methods that are listed above.
+# You can write method headers so the program will actually compile.
+# I recommend using a library called Tkinter (you can import it) but
+# if you find something that looks better you can use that instead.
