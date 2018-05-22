96d7818a3272aff26694b0577f4cf1f2e29e4409
James Steven <Jamessteven301@gmail.com>
2018-05-21 22:04:54 -0400

    Add files via upload

---
+++ contract-gui.py
@@ -0,0 +1,21 @@
+##########################################
+#-----------------gui.py-----------------#
+##########################################
+
+#-----------visual elements--------------#
+    #@button - lable: "Calibrate"
+        #Calls method: calibrate()
+    #@button - lable: "Execute"
+        #Calls method: execute()
+    #@button - lable: "Reset"
+        #Calls method: reset()
+    #@button - lable: "Interface"
+        #Calls method: interface()
+
+#-------------Description----------------#
+# just make these four buttons in a basic gui window.
+# all they have to do is call ghost methods that are listed above.
+# you can write method headers so the program will actually compile.
+# I recommend using a library called Tkinter (you can import it) but
+# if you find something that looks better you can use that instead.
+
