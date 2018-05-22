7e7eb8b2b452ba6d3cae1a3a371ec0782e6f518b
lukekoury <36520299+lukekoury@users.noreply.github.com>
2018-05-22 15:43:09 -0400

    Update gui.py
    basic gui

--- gui.py
+++ gui.py
@@ -1,1 +1,38 @@
+import tkinter as tk
+
+def calibrate():
+	print('calibrate')
+
+def execute():
+	print('execute')
+
+def reset():
+	print('reset')
+
+def interface():
+	print('interface')
+
+root = tk.Tk()
+frame = tk.Frame(root)
+frame.pack()
+
+
+#cB = calibrate Button
+cB = tk.Button(frame, text="Calibrate", command=calibrate)
+cB.pack(side=tk.LEFT)
+
+eB = tk.Button(frame, text="Execute", command=execute)
+eB.pack(side=tk.LEFT)
+
+rB = tk.Button(frame, text="Reset", command=calibrate)
+rB.pack(side=tk.LEFT)
+
+iB = tk.Button(frame, text="Interface", command=calibrate)
+iB.pack(side=tk.LEFT)
+
+qB = tk.Button(frame, text="QUIT", fg="red", command=quit)
+qB.pack(side=tk.LEFT)
+
+
+root.mainloop()
 
