0aedbdc8882e89c1cb6dab0a5ff20a610feee585
James Steven <Jamessteven301@gmail.com>
2018-05-22 11:54:35 -0400

    Update contract-gui.py

--- contract-gui.py
+++ contract-gui.py
@@ -13,9 +13,9 @@
         #Calls method: interface()
 
 #-------------Description----------------#
-# just make these four buttons in a basic gui window.
-# all they have to do is call ghost methods that are listed above.
-# you can write method headers so the program will actually compile.
+# Just make these four buttons in a basic gui window.
+# All they have to do is call ghost methods that are listed above.
+# You can write method headers so the program will actually compile.
 # I recommend using a library called Tkinter (you can import it) but
 # if you find something that looks better you can use that instead.
 
