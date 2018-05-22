e06f4d76cbf88f85437c7dac827ebf471e52e9eb
lukekoury <36520299+lukekoury@users.noreply.github.com>
2018-05-22 16:46:04 -0400

    Better GUI
    added colors and moved quit to bottom

--- gui.py
+++ gui.py
@@ -1,37 +1,41 @@
 import tkinter as tk
 
 def calibrate():
-	print('calibrate')
+	print('calibrate:\n')
 
 def execute():
-	print('execute')
+	print('execute:\n')
 
 def reset():
-	print('reset')
+	print('reset:\n')
 
 def interface():
-	print('interface')
+	print('interface:\n')
 
-root = tk.Tk()
-frame = tk.Frame(root)
-frame.pack()
+root = tk.Tk() #creates window
+Tframe = tk.Frame(root) #creates frame in window
+Tframe.pack() #packs frame into window
 
+Bframe = tk.Frame(root)
+Bframe.pack(side=tk.BOTTOM)
+
+root.title('Chess Robot')
 
 #cB = calibrate Button
-cB = tk.Button(frame, text="Calibrate", command=calibrate)
+cB = tk.Button(Tframe, text="Calibrate", bg="yellow", command=calibrate)
 cB.pack(side=tk.LEFT)
 
-eB = tk.Button(frame, text="Execute", command=execute)
+eB = tk.Button(Tframe, text="Execute", bg="green", command=execute)
 eB.pack(side=tk.LEFT)
 
-rB = tk.Button(frame, text="Reset", command=reset)
+rB = tk.Button(Tframe, text="Reset", bg="purple", command=reset)
 rB.pack(side=tk.LEFT)
 
-iB = tk.Button(frame, text="Interface", command=interface)
+iB = tk.Button(Tframe, text="Interface", bg="blue", command=interface)
 iB.pack(side=tk.LEFT)
 
-qB = tk.Button(frame, text="QUIT", fg="red", command=quit)
-qB.pack(side=tk.LEFT)
+qB = tk.Button(Bframe, text="QUIT", fg="red", command=quit)
+qB.pack()
 
 
 root.mainloop()
